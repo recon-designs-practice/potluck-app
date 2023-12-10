adc0473b8605a4d7054a3523267546ab8729c940
Stedman <michaelstedman81@gmail.com>
2023-12-10 17:08:44 -0600

    removed Next.js and with with React

--- app/components/header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {
-  children?: React.ReactNode;
-};
-
-export default function Header({ children }: Props) {
-  return <header>{!children ? <h3>Header</h3> : children}</header>;
-}
--- app/stores/store.ts
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useSetupStore = create((set) => ({
-  isUserLoggedIn: false,
-  setIsUserLoggedIn: (set: any) => (state: any) => ({ isUserLoggedIn: !state.isUserLoggedIn})
-}));
-
-export default useSetupStore

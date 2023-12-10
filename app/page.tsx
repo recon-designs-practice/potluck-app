5410d004d02e1cd8249a3c0d235118b8e43540a8
Stedman <michaelstedman81@gmail.com>
2023-12-10 12:50:52 -0600

    feat: added Zustand for state management

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+"use client"
+
+import React from "react";
+import useTestStore from "./stores/store";
 
 export default function Home() {
   return (
     <main>
       <h1>Home page</h1>
     </main>
-  )
+  );
 }

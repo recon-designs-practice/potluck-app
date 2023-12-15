dc1e8355ff8d80d44ebe2041d3234da838e9094a
Stedman <michaelstedman81@gmail.com>
2023-12-14 22:06:54 -0600

    feat: removed environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -9,12 +9,12 @@ import {
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: 'AIzaSyD4wuMPnPv6kUOOqnXvJGuopbZk93zo7JY',
+  authDomain: 'potluck-1ef67.firebaseapp.com',
+  projectId: 'potluck-1ef67',
+  storageBucket: 'potluck-1ef67.appspot.com',
+  messagingSenderId: '849384830952',
+  appId: '1:849384830952:web:501c44f536b9b03152d65d',
 };
 
 const app = initializeApp(firebaseConfig);
